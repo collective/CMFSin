36b876523068b868c5591342da1adb4c1f84866b
Benjamin Saller <bcsaller@gmail.com>
2002-10-31 16:22:31 +0000

    *added installer, show two maps on testpage

---
+++ Extensions/Install.py
@@ -0,0 +1,41 @@
+from Products.CMFSin import sin_globals
+from StringIO import StringIO
+from Products.CMFCore.utils import getToolByName
+
+SKIN_NAME = "sin"
+_globals = globals()
+
+def install_tools(self, out):
+    if not hasattr(self, "sin_tool"):
+        addTool = self.manage_addProduct['CMFSin'].manage_addTool
+        addTool('CMFSin Syndication Tool')
+
+
+def install_subskin(self, out, skin_name=SKIN_NAME, globals=sin_globals):
+    skinstool=getToolByName(self, 'portal_skins')
+    if skin_name not in skinstool.objectIds():
+        addDirectoryViews(skinstool, 'skins', globals)
+
+    for skinName in skinstool.getSkinSelections():
+        path = skinstool.getSkinPath(skinName) 
+        path = [i.strip() for i in  path.split(',')]
+        try:
+            if skin_name not in path:
+                path.insert(path.index('custom') +1, skin_name)
+        except ValueError:
+            if skin_name not in path:
+                path.append(skin_name)  
+
+        path = ','.join(path)
+        skinstool.addSkinSelection( skinName, path)
+
+def install(self):
+    out = StringIO()
+    print >>out, "Installing CMFSin"
+    
+    install_tools(self, out)
+    install_subskin(self, out)
+    
+    return out.getvalue()
+
+
--- __init__.py
+++ __init__.py
@@ -4,7 +4,8 @@ from Products.CMFCore.DirectoryView import registerDirectory
 import Products.CMFCore.utils
 
 ADD_CONTENT_PREMISSIONS = 'Manage Portal'
-registerDirectory('skins', globals())
+sin_globals = globals()
+registerDirectory('skins', sin_globals)
 
 PKG_NAME = "CMFSin"
 
