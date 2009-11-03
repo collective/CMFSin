cfde47264b97eccaf1a99314cfe9460fd7d264ae
Wouter Vanden Hove <wouter.vanden.hove@gmail.com>
2009-11-03 16:41:34 +0000

    remove product_name-parameter to avoid deprecation-warning (deprecated since CMF1.5 ~Plone2.1)

--- __init__.py
+++ __init__.py
@@ -1,5 +1,3 @@
-from AccessControl import ModuleSecurityInfo
-from Globals import InitializeClass
 from Products.CMFCore.DirectoryView import registerDirectory
 import Products.CMFCore.utils
 
@@ -10,11 +8,11 @@ registerDirectory('skins', sin_globals)
 PKG_NAME = "CMFSin"
 
 from Products.CMFSin.SinTool import SinTool
-tools = (SinTool,)
+tools = (SinTool, )
+
 
 def initialize(context):
     Products.CMFCore.utils.ToolInit(PKG_NAME + " Tool", tools=tools,
-                   product_name=PKG_NAME,
                    icon="tool.gif",
                    ).initialize(context)
 
